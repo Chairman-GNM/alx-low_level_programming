8817d0260e396c99ad4cae2ba8aabd9d2f4be272
Chairman GNM <Ngangbe@gmail.com>
2022-09-13 07:47:18 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number 
+ * @c: integer
+ * Return: last digit
+ */
+int print_last_digit(int c)
+{
+	c = c % 10;
+	if (c < 0)
+	{
+		c = c * -1;
+	}
+	_putchar('0' + c);
+	return (c);
+}
