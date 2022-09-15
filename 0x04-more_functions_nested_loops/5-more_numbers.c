460c7e8581b5a33810f85978f305444de58545d9
Chairman GNM <Ngangbe@gmail.com>
2022-09-15 07:51:42 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,20 @@
 
 void more_numbers(void)
 {
-	int x, y;
+	int num, row, count;
 
-	for (x = 0; x < 10; x++)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (y = 0; y < 15; y++)
+		for (count = 0; count <= 14; ++count)
+		{
+			num = count;
+			if (count > 9)
 			{
-				_putchar(y + '0');
+				_putchar(1 + 48);
+				num = count % 10;
 			}
-		_putchar('\n')
+			_putchar(num + 48);
+		}
+		_putchar('\n');
 	}
 }
