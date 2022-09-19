c4a263dc019a89e5726253e8ffa80d5d5c8ed221
Chairman GNM <Ngangbe@gmail.com>
2022-09-19 16:10:27 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,9 +11,10 @@ void rev_string(char *s)
 
 	char temp;
 
-	for (len = 0; s[len] != '\0'; len++);
+	for (len = 0; s[len] != '\0'; len++)
+		;
 	i = 0;
-	half =len / 2;
+	half = len / 2;
 	while (half--)
 	{
 		temp = s[len - i - 1];
