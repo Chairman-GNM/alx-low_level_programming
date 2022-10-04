25beaeb239b297df9e89ab649139a30b108630ae
Chairman-GNM <111054602+Chairman-GNM@users.noreply.github.com>
2022-10-04 14:15:22 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing the prototypes of all functions
- *       included in the static library libholberton.a.
- */
 
 int _putchar(char c);
 int _islower(int c);
@@ -29,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
